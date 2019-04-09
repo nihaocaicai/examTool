39462729f53f4edad11ad5cf61157040bbe7d9e7
nihaocaicai <41501401+nihaocaicai@users.noreply.github.com>
2019-04-09 11:10:04 +0800

    1

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,66 +1,21 @@
-// pages/index/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function () {
-
+    //获得diary组件
+    this.diary = this.selectComponent("#diary");
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  showDiary() {
+    this.diary.showDiary();
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  //取消事件
+  _error() {
+    console.log('你点击了取消');
+    this.diary.hideDiary();
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //确认事件
+  _success() {
+    console.log('你点击了确定');
+    this.diary.hideDiary();
   }
 })
