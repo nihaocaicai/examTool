9cf9958f26f140895695afd688ad2503d3e1ac95
Orainge <orainge04@qq.com>
2019-04-10 11:25:38 +0800

    菜单 V1.0
    完成菜单功能

--- pages/menu/menu.js
+++ pages/menu/menu.js
@@ -5,62 +5,76 @@ Page({
    * 页面的初始数据
    */
   data: {
+    motto: "考研路上，我们都不是孤独的。", //座右铭
+  },
 
+  /*点击 考研计划 按钮 */
+  clickPlan: function() {
+    console.log("考研计划")
+  },
+
+  /*点击 考研日记 按钮 */
+  clickDiary: function() {
+    console.log("考研日记")
+  },
+
+  /*点击 考研安排 按钮 */
+  clickSchedule: function() {
+    console.log("考研安排")
+  },
+
+  /*点击 设置 按钮 */
+  clickSetting: function() {
+    console.log("设置")
+  },
+
+  /*点击 错误反馈 按钮 */
+  clickFeedback: function() {
+    console.log("错误反馈")
+  },
+
+  /*点击 联系我们 按钮 */
+  clickContactUs: function() {
+    console.log("联系我们")
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
-  },
+  onLoad: function(options) {},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
-  },
+  onShow: function() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
+  onShareAppMessage: function() {}
 })
