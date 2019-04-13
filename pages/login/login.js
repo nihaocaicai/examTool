06956b074356c303875a853e93764bc806e130b6
Orainge <orainge04@qq.com>
2019-04-14 02:09:51 +0800

    login

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,66 +1,118 @@
 // pages/login/login.js
+var app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    hasAuth: false,
+  },
 
+  /* 点击按钮获取用户信息 监听器 */
+  getUserInfo: function(e) {
+    app.globalData.userInfo = e.detail.userInfo
+    app.getOpenID() /*获取用户ID*/
+    wx.switchTab({
+      url: '../index/index',
+    })
+  },
+
+  /* 用户授权 */
+  authorize: function() {
+    var that = this
+    // 判断用户是否授权
+    if (wx.canIUse('button.open-type.getUserInfo')) {
+      app.userInfoReadyCallback = res => {
+        that.data({
+          hasAuth: true,
+        })
+        //授权，进入主界面
+        wx.switchTab({
+          url: '../index/index',
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        lang: "zh_CN",
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          app.getOpenID()
+          that.data({
+            hasAuth: true,
+          })
+          //授权，进入主界面
+          wx.switchTab({
+            url: '../index/index',
+          })
+        }
+      })
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    if (app.globalData.userInfo) {
+      this.setData({
+        hasAuth: true,
+      })
+      //授权，进入主界面
+      wx.switchTab({
+        url: '../index/index',
+      })
+    }else{
+      this.authorize()
+    }
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
