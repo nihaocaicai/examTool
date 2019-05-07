bec643d97679c25deb42ddc05f912b4f53675f84
Orainge <orainge04@qq.com>
2019-05-07 17:44:09 +0800

    初步修改 exam

--- pages/menu/menu-content/exam/exam-model.js
+++ pages/menu/menu-content/exam/exam-model.js
@@ -1,138 +1,70 @@
-var app = getApp()
+import {
+  Request
+} from "../../../../utils/server/request.js"
+
 //var examData = require('../../../../data/local_exam_database.js')
 //wx.setStorageSync("exam_arrangement", examData.examList)
 
 class Exam {
-  setPage(page) {
-    this.page = page
+  /**
+   * [获取考研安排]
+   */
+  getAllArrangements(params) {
+    var r = new Request()
+    r.request({
+      url: '/user/arrangements/all',
+      success: params.success,
+      fail: params.fail,
+    })
   }
 
-  //获取所有计划
-  getAllArrangements() {
-    var that = this
-    wx.showLoading({
-      title: '加载中',
+  /**
+   * [添加考研安排]
+   */
+  addArramgements(params) {
+    var r = new Request()
+    r.request({
+      url: '/user/arrangements/add',
+      method: "POST",
+      data: params.data,
+      success: params.success,
+      statusCodeFail: params.statusCodeFail,
+      fail: params.fail,
     })
-    wx.request({
-      url: app.globalData.ip + app.globalData.interface.getAllArrangements,
+  }
+
+  /**
+   * [修改考研安排]
+   */
+  modifyArrangements(params) {
+    var r = new Request()
+    r.request({
+      url: '/user/arrangements/modify',
+      method: "POST",
+      data: params.data,
+      success: params.success,
+      statusCodeFail: params.statusCodeFail,
+      fail: params.fail,
+    })
+  }
+
+  /**
+   * [删除考研安排]
+   */
+  deleteArrangements(params) {
+    var r = new Request()
+    r.request({
+      url: '/user/arrangements/delete',
       data: {
-        token: wx.getStorageSync("wx_user_info").token
+        arrange_id: params.id
       },
-      success: function(res) {
-        if (res.statusCode == 200) {
-          try {
-            wx.setStorageSync('exam_arrangement', res.data.exam_arrangement)
-            that.getArrangementsFromCache()
-            wx.hideLoading()
-          } catch (e) {
-            var errMsg = "存储到缓存错误:\n" + e
-            that.isOffline(0, errMsg)
-          }
-        } else {
-          var errMsg = "服务器错误，返回代码: " + res.statusCode
-          that.isOffline(0, errMsg)
-        }
-      },
-      fail: function(res) {
-        var errMsg = "网络连接失败\n" + res
-        that.isOffline(0, errMsg)
-      }
-
+      success: params.success,
+      statusCodeFail: params.statusCodeFail,
+      fail: params.fail,
     })
-  }
-
-  //下拉添加
-  addItem() {
-    this.page.editexam.setData({
-      isModify: false,
-    })
-    this.page.editexam.showEdit()
-  }
-
-  //点击修改
-  modItem(index) {
-    var data = this.page.data.examList[index.dayindex]['data'][index.index]
-    this.modify_arrangement_id = this.page.data.examList[index.dayindex]['data'][index.index].arrangement_id
-
-    //日期和时间被放在一起保存了，所以需要分开
-    var examDateAndTime = data.arrangement_time.split(" ")
-    var promptDateAndTime = data.plan_if_prompt_time.split(" ")
-
-    this.page.editexam.setData({
-      isModify: true,
-      arrangement_id: data.arrangement_id,
-      arrangement_content: data.arrangement_content,
-      arrangement_place: data.arrangement_place,
-      plan_if_prompt: data.plan_if_prompt,
-      arrangement_date: examDateAndTime[0],
-      arrangement_time: examDateAndTime[1],
-      plan_if_prompt_date: promptDateAndTime[0],
-      plan_if_prompt_time: promptDateAndTime[1],
-    })
-    this.page.editexam.showEdit()
-  }
-
-  //点击删除
-  delItem(index) {
-    //index: [0,0]
-    wx.showModal({
-      title: '提示',
-      content: '你确定要删除这项计划吗？\r\n',
-      confirmColor: "#04838e",
-      success(res) {
-        if (res.confirm) {
-          //点击确认
-          console.log("确认删除")
-        }
-      }
-    })
-  }
-
-  //修改保存
-  modify_confirm(formData) {
-    console.log("修改")
-    formData.arrangement_id = this.modify_arrangement_id
-    console.log(formData)
-  }
-
-  //添加保存
-  add_confirm(formData) {
-    console.log("添加")
-    console.log(formData)
-  }
-
-  //从缓存获取用户安排
-  getArrangementsFromCache() {
-    var arrangements = wx.getStorageSync('exam_arrangement')
-    if (arrangements != undefined) {
-      this.page.setData({
-        examList: arrangements,
-        showView: arrangements.length != 0,
-        loading: false,
-      })
-    }
-  }
-
-  //当前为离线模式
-  isOffline(fromFlag, errMsg) {
-    var t = "离线提示来源标签:" + fromFlag.toString() + "\n--错误信息--\n" + errMsg
-    console.log(t)
-    //提示离线模式
-    if (!wx.getStorageSync('hideOfflineTips')) {
-      if (fromFlag == 0) {
-        //获取计划时失败的提示
-        this.getArrangementsFromCache()
-        wx.hideLoading()
-        wx.showToast({
-          title: '当前为离线模式',
-          image: "/images/login_fail.png",
-          duration: 1800,
-        })
-      }
-    }
   }
 }
 
 export {
   Exam
-};
+}
--- pages/menu/menu-content/exam/exam.js
+++ pages/menu/menu-content/exam/exam.js
@@ -1,8 +1,12 @@
-// pages/menu/menu-content/exam/exam.js
 import {
   Exam
 } from "exam-model.js"
-var exam = new Exam()
+
+import {
+  Storage
+} from "../../../../utils/storage.js"
+
+var model = new Exam()
 
 Page({
   data: {
@@ -11,49 +15,230 @@ Page({
     loading: true,
   },
 
-  onLoad: function(options) {
-    exam.setPage(this)
-  },
-
   onReady: function() {
-    this.editexam = this.selectComponent("#editexam"); //获得edit组件
-    exam.getAllArrangements()
+    this._initData()
   },
 
-  //下拉添加
+  /* 事件响应 */
+  /**
+   * [事件_下拉页面]
+   * 添加计划
+   */
   onPullDownRefresh: function() {
-    exam.addItem()
+    if (!this.editexam)
+      this.editexam = this.selectComponent("#editexam") //获得edit组件
+
+    this.editexam.setData({
+      isModify: false,
+    })
+    this.editexam.showEdit()
     wx.stopPullDownRefresh()
   },
 
-  //点击修改
+  /**
+   * [事件_点击修改]
+   */
   modItem: function(e) {
     var index = e.currentTarget.dataset
-    exam.modItem(index)
+    var data = this.data.examList[index.dayindex]['data'][index.index]
+    this.data.modify_arrangement_id = this.data.examList[index.dayindex]['data'][index.index].arrangement_id
+
+    if (!this.editexam)
+      this.editexam = this.selectComponent("#editexam") //获得edit组件
+
+    this.editexam.setData({
+      isModify: true,
+      arrangement_id: data.arrangement_id,
+      arrangement_content: data.arrangement_content,
+      arrangement_place: data.arrangement_place,
+      plan_if_prompt: data.plan_if_prompt,
+      arrangement_date: data.arrangement_date,
+      arrangement_time: data.arrangement_time,
+      plan_if_prompt_date: data.plan_if_prompt_date,
+      plan_if_prompt_time: data.plan_if_prompt_time,
+    })
+    this.editexam.showEdit()
   },
 
-  //点击删除
+  /**
+   * [事件_点击删除]
+   */
   delItem: function(e) {
-    var index = e.currentTarget.dataset
-    exam.delItem(index)
+    var that = this
+    var index = e.currentTarget.dataset //index: {dayindex: "0", index: "0"}
+    wx.showModal({
+      title: '提示',
+      content: '你确定要删除这项计划吗？\r\n',
+      confirmColor: "#04838e",
+      success(res) {
+        if (res.confirm) {
+          //确认删除
+          model.deleteArrangements({
+            id: that.data.examList[index.dayindex]['data'][index.index].arrangement_id,
+            success: function() {
+              that._removeItemFromList(index.dayindex, index.index)
+            },
+            statusCodeFail: function() {
+              wx.hideLoading()
+              that._errorServer()
+            },
+            fail: function() {
+              wx.hideLoading()
+              that.isOffline()
+            }
+          })
+        }
+      }
+    })
   },
 
-  //修改保存
+  /**
+   * [事件_修改对话框_确定]
+   */
   modify_confirm: function(e) {
-    exam.modify_confirm(e.detail)
+    var formData = e.detail
+    formData.arrangement_id = this.data.modify_arrangement_id
+    console.log("修改")
+    console.log(formData)
   },
 
-  //添加保存
+  /**
+   * [事件_添加对话框_确定]
+   */
   add_confirm: function(e) {
-    exam.add_confirm(e.detail)
+    var formData = e.detail
+    console.log(formData)
+    console.log("添加")
+    console.log(formData)
   },
 
-  //点击取消按钮
+  /**
+   * [事件_对话框_取消]
+   */
   hidden_dialog: function() {
     //暂时不需要实现
   },
 
-  //滑动组件start
+  /**
+   * [初始化数据]
+   */
+  _initData() {
+    var that = this
+    wx.showLoading({
+      title: '加载中',
+    })
+    model.getAllArrangements({
+      success: function(data) {
+        var saveData = data
+        var s = new Storage()
+        s.save({
+          key: 'exam_arrangement',
+          data: saveData, //保存服务器获取到的数据到微信缓存
+          success: function() {
+            that._getArrangementFromStorage()
+          },
+          fail: function() {
+            that._getArrangementFromStorage(true)
+          },
+        })
+      },
+      fail: function() {
+        that._getArrangementFromStorage(true)
+      },
+    })
+  },
+
+  /**
+   * [从缓存读取信息]
+   */
+  _getArrangementFromStorage(isOffline) {
+    var arrangements = wx.getStorageSync('exam_arrangement')
+    if (arrangements != undefined) {
+      this.setData({
+        examList: arrangements,
+        showView: arrangements.length != 0,
+        loading: false,
+      })
+    }
+    wx.hideLoading()
+    if (isOffline) {
+      if (!wx.getStorageSync('hideOfflineTips')) {
+        wx.showToast({
+          title: '当前为离线模式',
+          image: "/images/login_fail.png",
+          duration: 1800,
+        })
+      }
+    }
+  },
+
+  /**
+   * [从列表中移除指定的项目]
+   */
+  _removeItemFromList(dayindex, index) {
+    var that = this
+    var newList = that.data.examList
+    newList[dayindex].data.splice(index, 1) //删除制定项目
+    if (newList[dayindex].data.length == 0) {
+      //如果删除项目后该日期的数组长度为，移除该日期
+      newList.splice(dayindex, 1)
+    }
+    var s = new Storage()
+    s.save({
+      key: 'exam_arrangement',
+      data: newList, //新的列表
+      success: function() {
+        that.setData({
+          examList: wx.getStorageSync('exam_arrangement'),
+          lastScroll: [-1, -1], //重设上一个滑出的项
+        })
+        wx.hideLoading()
+      },
+      fail: function() {
+        that._errorSave()
+        wx.hideLoading()
+      },
+    })
+  },
+
+  /**
+   * [服务器错误提示]
+   */
+  _errorServer() {
+    wx.showModal({
+      title: '提示',
+      content: '服务器出错，请稍后重试',
+      showCancel: false,
+      confirmColor: "#04838e",
+    })
+  },
+
+  /**
+   * [离线错误提示]
+   * type: 操作类型
+   */
+  _isOffline(type) {
+    wx.showModal({
+      title: '提示',
+      content: '当前为离线模式，不能进行' + type + '操作',
+      showCancel: false,
+      confirmColor: "#04838e",
+    })
+  },
+
+  /**
+   * [信息保存失败提示]
+   */
+  _errorSave() {
+    wx.showModal({
+      title: '提示',
+      content: '操作失败,请稍后重试',
+      showCancel: false,
+      confirmColor: "#04838e",
+    })
+  },
+
+  /* 滑动组件*/
   drawStart: function(e) {
     this.setData({
       startX: e.touches[0].clientX,
@@ -83,12 +268,6 @@ Page({
         isScroll: false,
         examList: this.data.examList,
       })
-    } else {
-      item.right = 0
-      this.setData({
-        isScroll: true,
-        examList: this.data.examList
-      })
     }
   },
 
@@ -101,14 +280,7 @@ Page({
         examList: this.data.examList,
         lastScroll: [e.currentTarget.dataset.dayindex, e.currentTarget.dataset.index]
       })
-    } else {
-      item.right = 0
-      this.setData({
-        isScroll: true,
-        examList: this.data.examList,
-        lastScroll: [-1, -1]
-      })
     }
   },
-  //滑动组件end
+  /* 滑动组件end*/
 })
